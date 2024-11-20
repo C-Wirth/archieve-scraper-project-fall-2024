2d838992f08954ff0a80941eb7b838f3d21d7691
C-Wirth <151962189+C-Wirth@users.noreply.github.com>
2024-11-20 17:15:51 -0500

    db added

--- pipeline/_2_db_builder.py
+++ pipeline/_2_db_builder.py
@@ -1,3 +1,5 @@
+
+import httpx
 from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
@@ -5,7 +7,12 @@ from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
 from langchain_ollama import OllamaLLM
 
+import chromadb.utils.embedding_functions as embedding_functions
 from _1_WikiLeaksScraper import POST_DOWNLOAD_END_DELIMITER
+
+import ollama
+
+import chromadb
 
 import shutil
 import os
@@ -18,6 +25,8 @@ DATA_PATH = f"{INPUT_REPO}"
 MODEL="llama3.2"
 CHROMA_PATH = "chroma"
 
+
+
 text_splitter = RecursiveCharacterTextSplitter( #Splitter splits an email into chunks by paragraphs
     chunk_size=1000,
     separators= [
@@ -29,7 +38,13 @@ text_splitter = RecursiveCharacterTextSplitter( #Splitter splits an email into c
 
 #python3 pipeline/_2_db_builder.py
 
-embedding_function = OllamaEmbeddings(model=MODEL,)
+# embedding_function = OllamaEmbeddings(model=MODEL,) 
+
+#Try new embedding function
+embedding_function = embedding_functions.OllamaEmbeddingFunction( 
+    url="http://localhost:11434/api/embeddings",
+    model_name="llama3.2",
+)
 
 llm = OllamaLLM(model=MODEL)
 
@@ -43,18 +58,28 @@ def main():
 
     chroma_builder(all_chunks)
 
-def chroma_builder(all_chunks: list[Document]):
+def chroma_builder(documents: list[Document]):
 
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
 
-    db = Chroma.from_documents(
-        all_chunks,
-        embedding_function,
-        persist_directory=CHROMA_PATH
-    )
-    print("Database successfully created")
-    #db.
+    client = chromadb.PersistentClient(path=CHROMA_PATH)
+
+    print("Empty Database successfully created")
+
+    collection = client.create_collection("emails")
+
+    for i, d in enumerate(documents):
+        response = ollama.embeddings(model="mxbai-embed-large", prompt=d.page_content)
+        embedding = response["embedding"]
+        collection.add(
+            ids=[str(i)],
+            embeddings=[embedding],
+            documents=[d.page_content]
+        )
+        print(f"Document {i} added")
+
+
 def buildChunks(documents):
 
     chunks = []
@@ -66,8 +91,6 @@ def buildChunks(documents):
 
     print(f"Processing Completed: {total_docs} documents added as {len(chunks)} chunks\n") 
     return chunks
-
-
 
 def load_documents(): #The document contains all content on a page and meta data
     
